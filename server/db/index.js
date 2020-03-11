abc5d9e49cce747d6101647fa30a5b41f8ea7204
oriluka <mariahtato@gmail.com>
2020-03-11 16:14:20 -0700

    Added basic db details and defined a model

---
+++ server/db/index.js
@@ -0,0 +1,30 @@
+var Sequelize = require('sequelize');
+
+// Will put db name here at some point
+var dbName = '';
+var username = 'root';
+var password = ''
+
+// Databse login info
+var db = new Sequelize(dbName, username, password, {
+  dialect: "mysql",
+  port: 3000
+});
+
+// Connect to database
+db
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established')
+  }, function (err) {
+    console.log('Unable to connect to the database: ', err)
+  });
+
+// Defining a model. Id, createdAt and updateAt auto added.
+var Artist = db.define('Artist', {
+  name: Sequelize.STRING,
+  track_count: Sequelize.INTEGER,
+  follower_count: Sequelize.INTEGER,
+  following_count: Sequelize.INTEGER,
+  links: Sequelize.STRING
+})
