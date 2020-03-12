6c71a97d89b0f4a36ef712f77f016976d08db2da
oriluka <mariahtato@gmail.com>
2020-03-12 11:14:04 -0700

    Finished setup, prob going to switch from request to jQeury ajax

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import request from 'request';
+import StatCounts from './StatCounts.jsx';
+import Links from './Links.jsx';
+import About from './About.jsx';
+
 
 class Sidebar extends React.Component {
 
@@ -19,13 +23,15 @@ class Sidebar extends React.Component {
   componentWillMount() {
     // get req here
     that = this;
+
     request
+      // If continuing to use request then url is going to be the parameter.
       .get('/artist')
       .on('response', function(artist) {
         // add the values to the state
-        that.setState(artist, function() { console.log('State has been changed!')})
-      })
-
+        that.setState(artist, function() { console.log('State has been changed!')};);
+      });
+  }
 
   // on click function to be added when I have time
 
