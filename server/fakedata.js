0d56206aed6020ee464845552623fa6e216815fe
oriluka <mariahtato@gmail.com>
2020-03-12 09:14:24 -0700

    Created a fakedata file to create fake data for server

--- server/fakedata.js
+++ server/fakedata.js
@@ -1,10 +1,35 @@
-// import request
 var request = require('request');
-// imort faker
-//var faker = require('faker');
+var faker = require('faker');
+//var bodyParser = require('body-parser');
+var db = require('./db/index.js');
 
 
-// create get req to get data ============
+// Create random data object and values
+var fakeInfo = {about: ''};
+
+// Random name type generator
+var nameType = Math.floor(Math.random() * 5);
+if (nameType === 0) {
+  var randomName = faker.finance.accountName();
+}
+if (nameType === 1 || nameType === 2) {
+  var randomName = faker.internet.userName();
+} else {
+  var randomName = faker.name.findName();
+}
+
+// Generate rrest of fake data
+var fakeLinks = faker.internet.url();
+var trackCount = Math.ceil(Math.random() * 200);
+var followerCount = Math.floor(Math.random() * 20000);
+var followingCount = Math.floor(Math.random() * 1000);
+
+// Add fake data to fakeInfo object
+fakeInfo.name = randomName;
+fakeInfo.track_count = trackCount;
+fakeInfo.follower_count = followerCount;
+fakeInfo.following_count = followingCount;
+fakeInfo.links = fakeLinks;
 
 // == Randomize the get statement
 var sentences = Math.floor(Math.random() * 2);
@@ -14,22 +39,26 @@ if (sentences === 0) {
 } else {
   var parameter = 'paras';
 }
-// set fake info object
-var fakeInfo = {about: ''};
-
+// Create fake info object
 // get about
+
 request
   .get(`https://hipsum.co/api/?type=hipster-centric&${parameter}=${num}`, function(err, response, body) {
     if (err) {
       console.log('An error has occurred trying to retrieve hipster data');
-    }
-    else {
-      // body will be an array
-      console.log(body);
+
+    } else {
+    // body will be an array, turn to string
+      fakeInfo.about = body;
+      // add this data to tables
+      db.create(fakeInfo)
+        .then(function (artist) {
+          console.log('New artist entry has been added to database');
+          //console.log(artist);
+        })
+        .catch(function (err) {
+          console.log('An error occurred trying to add new artist to the database');
+        //console.log(err);
+        });
     }
   });
-
-// get links
-
-
-// add this data to tables
