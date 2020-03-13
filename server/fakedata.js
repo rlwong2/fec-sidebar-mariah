69b940a9a7294fe11ceff43cec27981ff066dbf9
oriluka <mariahtato@gmail.com>
2020-03-12 19:42:37 -0700

    Reworked to be a callable function;

--- server/fakedata.js
+++ server/fakedata.js
@@ -4,61 +4,68 @@ var faker = require('faker');
 var db = require('./db/index.js');
 
 
-// Create random data object and values
-var fakeInfo = {about: ''};
+const createFakeArtist = function() {
 
-// Random name type generator
-var nameType = Math.floor(Math.random() * 5);
-if (nameType === 0) {
-  var randomName = faker.finance.accountName();
-}
-if (nameType === 1 || nameType === 2) {
-  var randomName = faker.internet.userName();
-} else {
-  var randomName = faker.name.findName();
-}
 
-// Generate rrest of fake data
-var fakeLinks = faker.internet.url();
-var trackCount = Math.ceil(Math.random() * 200);
-var followerCount = Math.floor(Math.random() * 20000);
-var followingCount = Math.floor(Math.random() * 1000);
+  // Create random data object and values
+  var fakeInfo = {about: ''};
 
-// Add fake data to fakeInfo object
-fakeInfo.name = randomName;
-fakeInfo.track_count = trackCount;
-fakeInfo.follower_count = followerCount;
-fakeInfo.following_count = followingCount;
-fakeInfo.links = fakeLinks;
+  // Random name type generator
+  var nameType = Math.floor(Math.random() * 5);
+  if (nameType === 0) {
+    var randomName = faker.finance.accountName();
+  }
+  if (nameType === 1 || nameType === 2) {
+    var randomName = faker.internet.userName();
+  } else {
+    var randomName = faker.name.findName();
+  }
 
-// == Randomize the get statement
-var sentences = Math.floor(Math.random() * 2);
-var num = Math.ceil(Math.random() * 3);
-if (sentences === 0) {
-  var parameter = 'sentences';
-} else {
-  var parameter = 'paras';
-}
-// Create fake info object
-// get about
+  // Generate rrest of fake data
+  var fakeLinks = faker.internet.url();
+  var trackCount = Math.ceil(Math.random() * 200);
+  var followerCount = Math.floor(Math.random() * 20000);
+  var followingCount = Math.floor(Math.random() * 1000);
 
-request
-  .get(`https://hipsum.co/api/?type=hipster-centric&${parameter}=${num}`, function(err, response, body) {
-    if (err) {
-      console.log('An error has occurred trying to retrieve hipster data');
+  // Add fake data to fakeInfo object
+  fakeInfo.name = randomName;
+  fakeInfo.track_count = trackCount;
+  fakeInfo.follower_count = followerCount;
+  fakeInfo.following_count = followingCount;
+  fakeInfo.links = fakeLinks;
 
-    } else {
-    // body will be an array, turn to string
-      fakeInfo.about = body;
-      // add this data to tables
-      db.create(fakeInfo)
-        .then(function (artist) {
-          console.log('New artist entry has been added to database');
-          //console.log(artist);
-        })
-        .catch(function (err) {
-          console.log('An error occurred trying to add new artist to the database');
-        //console.log(err);
-        });
-    }
-  });
+  // == Randomize the get statement
+  var sentences = Math.floor(Math.random() * 2);
+  var num = Math.ceil(Math.random() * 3);
+  if (sentences === 0) {
+    var parameter = 'sentences';
+  } else {
+    var parameter = 'paras';
+  }
+
+  // Create fake about object
+  request
+    .get(`https://hipsum.co/api/?type=hipster-centric&${parameter}=${num}`, function(err, response, body) {
+      if (err) {
+        console.log('An error has occurred trying to retrieve hipster data');
+
+      } else {
+      // body will be an array, turn to string
+        fakeInfo.about = body;
+        // add this data to tables
+        db.create(fakeInfo)
+          .then(function (artist) {
+            console.log('New artist entry has been added to database');
+            //console.log(artist);
+
+            // we run the fakesongLikes function
+
+          })
+          .catch(function (err) {
+            console.log('An error occurred trying to add new artist to the database');
+          //console.log(err);
+          });
+
+};
+
+module.exports = createFakeArtist()
