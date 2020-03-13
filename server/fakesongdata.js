e60728b2d4a2a304604031e03a19035cadbbd25c
oriluka <mariahtato@gmail.com>
2020-03-12 19:18:48 -0700

    Set up functions to create fakeData still missing calls to get media images for albumArt

---
+++ server/fakesongdata.js
@@ -0,0 +1,90 @@
+var request = require('request');
+var faker = require('faker');
+//var bodyParser = require('body-parser');
+var db = require('./db/index.js');
+
+const generateAPIinfo = function(username) {
+
+  // Create random data object and values
+  var fakeInfo = { about: '' };
+
+  // generate random song names
+
+
+  // generate random numbers to help randomize artist name
+  // generate random artist name
+  axios.get(`http://names.drycodes.com/1?nameOptions=funnyWords`)
+    .then(function (response) {
+      var num = Math.ceil(Math.random() * 2)
+      var fakeArtistName = response[0]
+      var temp = fakeArtistName.split('_')
+      if (num = 1) {
+        fakeArtistnName = temp[0]
+      } else {
+        fakeArtistName = temp.join(' ')
+      }
+
+    axios.get(`http://names.drycodes.com/1?separator=space`)
+    .then(function (response) {
+      var fakeSongName = response[0]
+
+      // run the rest of the random info generator
+      generateRandomLikeData(fakeArtistName, fakeSongName, fakeAlbumArt);
+    })
+    .catch(function (error) {
+      console.log('There was an error retrieving your fake song name ')
+    })
+
+    })
+    .catch(function (error) {
+      console.log('There was an error retrieving your fake song name ')
+    })
+
+  // for every  1 like a song usually has 50 - 80 listens
+  // for every 1 rt a song usually has 10 - 30 likes
+  // comments  is really random
+}
+
+  // Generate rrest of fake data
+  var generateRandomLikeData = function (fakeArtistName, fakeSongName, fakeAlbumArt) {
+
+    var fakeUser = username
+    var fakeSongName =
+    var fakeArtistName = nameType;
+    var fakePlays = Math.ceil(Math.random() * 90000000);
+    var fakeLikes = fakeLikes / (50 * (Math.ceil(Math.random * 1.5)));
+    var fakeReposts = fakeReposts / (10 * (Math.ceil(Math.random * 3)));
+    var fakeComments = fakeReposts / (Math.random() * 5) + .5
+    var fakeAlbumArt = //got to get links
+
+    // Add fake data to fakeInfo object
+    fake.user = fakeUser
+    fake.song_name = fakeSongName
+    fake.artist_name = fakeArtistName
+    fake.plays = fakePlays
+    fake.likes = fakeLikes
+    fake.reposts = fakeReposts
+    fake.comments = fakeComments
+    fake.album_art = fakeAlbumArt
+
+  }
+
+  // Randomize get statement
+  // Create fake info object
+  // get about
+
+
+
+
+const startGenerateRandomSongLikes = function(username) {
+
+  var i = Math.floor(Math.random() * 20)
+
+  // this might not be async
+  while (i > 0) {
+    generateAPIinfo(username, function() )
+    i --;
+  }
+}
+
+module.exports = startGenerateRandomSongLikes;
