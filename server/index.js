c34cb94c5a92c8782590121f109d3ec7377ad8cb
oriluka <mariahtato@gmail.com>
2020-03-18 11:39:08 -0700

    Added React components for sidebar

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var path = require('path');
-var PORT = 3333;
+var PORT = 3334;
 // import artist model methods
 var db = require('./db/index.js');
 
